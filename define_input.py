aee53d9a0e7ed35b578b55559dba27423e342948
Rongxin Feng <58085834+xin5835@users.noreply.github.com>
2022-04-09 23:21:01 +0800

    Update and rename 3d_plot.py to define_input.py

---
+++ define_input.py
@@ -0,0 +1,9 @@
+# define a input program
+
+a_input=int(input('please give a number:'))
+if a_input == 1:
+    print('you are the best')
+elif a_input == 2:
+    print('not bad')
+else:
+    print('good luck')
